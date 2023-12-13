37043ec4dcef30cf8eaffa662d2daca88e4cf80e
luan-antonio <46446054+luan-antonio@users.noreply.github.com>
2023-12-13 15:15:35 -0300

    Update Greet component and Greet component test

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -6,3 +6,9 @@ test("Greet renders correctly", () => {
   const text = screen.getByText(/hello/i);
   expect(text).toBeInTheDocument();
 });
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Luan" />);
+  const text = screen.getByText("Hello Luan");
+  expect(text).toBeInTheDocument();
+});
